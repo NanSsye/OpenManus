58d3c4361a11540582b867dd9c44a9553e45d924
NanSsye <169164040+NanSsye@users.noreply.github.com>
2025-04-04 08:28:02 +0800

    Update __init__.py

--- tools/__init__.py
+++ tools/__init__.py
@@ -6,10 +6,16 @@ from .calculator_tool import CalculatorTool
 from .datetime_tool import DateTimeTool
 from .search_tool import SearchTool
 from .weather_tool import WeatherTool
+from .code_tool import CodeTool
+from .stock_tool import StockTool
+from .drawing_tool import ModelScopeDrawingTool
 
 __all__ = [
     "CalculatorTool",
     "DateTimeTool",
     "SearchTool",
-    "WeatherTool"
+    "WeatherTool",
+    "CodeTool",
+    "StockTool",
+    "ModelScopeDrawingTool",
 ] 
